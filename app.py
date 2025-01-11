1801e6e5dd043381a1c589ba2a03a152a9b7f0db
xash1rama <vladik.asa1998@gmail.cim>
2025-01-11 05:13:25 +0300

    last

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from starlette.responses import HTMLResponse, FileResponse
 from pathlib import Path
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 from routers.rout_build import router as build_rout
 from routers.rout_activity import router as activity_rout
 from routers.rout_organization import router as organization_rout
@@ -12,8 +13,10 @@ app.include_router(build_rout)
 app.include_router(activity_rout)
 app.include_router(organization_rout)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    file_path = Path("static/image.png")
-    return FileResponse(file_path, status_code=200)
+    file_path = Path("static/main.html")
+    return FileResponse(file_path, media_type='text/html')
